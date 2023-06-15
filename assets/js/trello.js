00e84482591df59748a13d834f967474b3d43bda
FDHoho007 <mail@fdhoho007.de>
2023-06-15 07:42:41 +0200

    Initial commit

---
+++ assets/js/trello.js
@@ -0,0 +1,6 @@
+function trello_authorize() {
+    
+}
+
+// https://trello.com/1/authorize?response_type=token&key=696f8e34d0af4d8f0583c1ad82bd932b&redirect_uri=file%3A%2F%2F%2Fhome%2Ffdhoho007%2Fgit%2Fgithub.com%2Ffdhoho007%2Ftrello2gitlab%2Findex.html&callback_method=fragment&scope=read&expiration=1hour&name=Trello2GitLab%20Migrator
+// https://api.trello.com/1/members/me/boards?key=696f8e34d0af4d8f0583c1ad82bd932b&token=
