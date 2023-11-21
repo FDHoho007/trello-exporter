ac57621cdcd49b58a61e40e8654ebf304d03f633
FDHoho007 <mail@fdhoho007.de>
2023-11-21 10:49:43 +0100

    Attachment workaround

--- assets/app.js
+++ assets/app.js
@@ -2,6 +2,7 @@ const TRELLO_API = "https://api.trello.com/1";
 const TRELLO_API_PARALLEL_LIMIT = 300/2;
 const TRELLO_API_PARALLEL_TIME_LIMIT = 10*1000;
 const TRELLO_API_KEY = "5bb9050954a2a56e7a5f22bc41c76b3a";
+const RETURN_URL = "https://students.fim.uni-passau.de/~dietrich/trello-export/";
 let TRELLO_API_TOKEN = null;
 
 if(location.hash) {
@@ -15,7 +16,7 @@ if(location.hash) {
 }
 
 if(TRELLO_API_TOKEN == null)
-    location.href = "https://students.fim.uni-passau.de/~dietrich/trello-export/";
+    location.href = RETURN_URL;
 else {
     const trello_boards = document.getElementById("trello-boards");
     trello_boards.innerHTML = "";
@@ -42,9 +43,13 @@ function hide_all() {
 }
 
 async function api(url) {
-    let response = await fetch(TRELLO_API + url + (url.includes("?") ? "&" : "?") +"key=" + TRELLO_API_KEY + "&token=" + TRELLO_API_TOKEN);
+    let response = await fetch((url.startsWith("https://") ? "" : TRELLO_API) + url, {
+        headers: {
+            "Authorization": "OAuth oauth_consumer_key=\"" + TRELLO_API_KEY + "\", oauth_token=\"" + TRELLO_API_TOKEN + "\""
+        }
+    });
     if(response.status != 200)
-        location.href = "https://students.fim.uni-passau.de/~dietrich/trello-export/";
+        location.href =  RETURN_URL;
     return await response.json();
 }
 
@@ -53,15 +58,6 @@ async function exportBoard(board) {
     document.getElementById("convert").style.display = "block";
     function board_api(url) {
         return api("/boards/" + board["id"] + url);
-    }
-    async function fetch_all(url) {
-        let page = 0;
-        let result = [], lastResult = null;
-        do {
-            lastResult = await board_api(url + "?limit=1000&page=" + page++);
-            result.push(...lastResult);
-        } while(lastResult.length == 1000);
-        return result;
     }
     board["actions"] = [];
     board["cards"] = await board_api("/cards/all"); // misses stuff
@@ -91,6 +87,7 @@ async function exportBoard(board) {
     board["customFields"] = await board_api("/customFields");
     board["memberships"] = await board_api("/memberships");
     board["pluginData"] = await api("/organizations/" + board["idOrganization"] + "/pluginData");
+    let download_attachments = 0;
     let i = 0;
     for(card of board["cards"]) {
         card["address"] = null;
@@ -114,6 +111,7 @@ async function exportBoard(board) {
                 "perCard": board["limits"]["stickers"]["perCard"]
             }
         };
+        download_attachments += card["attachments"].filter(a => a["isUpload"]).length;
         let lastActions = [];
         let page = 0;
         do {
@@ -140,5 +138,9 @@ async function exportBoard(board) {
         document.body.removeChild(a);
         window.URL.revokeObjectURL(url);  
     }, 0); 
+    document.getElementById("attachment-download").style.display = download_attachments > 0 ? "" : "none";
+    document.getElementById("attachment-download-count").innerText = download_attachments;
+    if(download_attachments > 0)
+        document.getElementById("attachment-download-token").innerText = TRELLO_API_TOKEN;
     return board;
 }
